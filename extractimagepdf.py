088ad59f3bf620ff8a7a9c14ae3b4c403998eed1
satheesh22g <satheeshgajula22.com>
2021-01-04 15:07:57 +0530

    committed

---
+++ extractimagepdf.py
@@ -0,0 +1,13 @@
+import fitz
+doc = fitz.open("file.pdf")
+for i in range(len(doc)):
+    for img in doc.getPageImageList(i):
+        xref = img[0]
+        pix = fitz.Pixmap(doc, xref)
+        if pix.n < 5:       # this is GRAY or RGB
+            pix.writePNG("image%s-%s.png" % (i, xref))
+        else:               # CMYK: convert to RGB first
+            pix1 = fitz.Pixmap(fitz.csRGB, pix)
+            pix1.writePNG("image%s-%s.png" % (i, xref))
+            pix1 = None
+        pix = None
